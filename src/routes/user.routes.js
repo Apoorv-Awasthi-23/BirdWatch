9450a52081214b2d169fdfbc0e2b771ac9c34aec
Apoorv <awasthiapoorv23@gmail.com>
2024-04-17 06:11:27 +0530

    refreshToken and access Tokens endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import {
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -20,8 +25,11 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
+
+//secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 
+router.route("/refresh-token").post(refreshAccessToken);
 
 
 export default router;
